8bb846dbf9fa6aae11b3dbc1f7deb890aa03081a
Michael Hansen <michaelhansen865@gmail.com>
2024-01-07 20:37:42 -0700

    testing branches

--- rockPapperScissors.js
+++ rockPapperScissors.js
@@ -15,36 +15,28 @@ function playRound(computerSelection, playerSelection) {
     computerSelection = cleanUpInput(computerSelection);
     playerSelection = cleanUpInput(playerSelection); 
     if (!isValid(computerSelection) || !isValid(playerSelection)){
-        console.log(`error choice invalid: ${computerSelection}
-            or ${playerSelection}`);
+        console.log(`error choice invalid: 
+            ${computerSelection} or ${playerSelection}`);
     }
 
-    //determine winner    
-    let winner;
-    switch (computerSelection+playerSelection){
-        case ('rock'+'scissor'): winner = 'computer';break;
-        case ('paper'+'rock'): winner ='computer';break;
-        case ('scissor'+'paper'): winner = 'computer';break;
-
-        case ('scissor'+'rock'): winner = 'player';break;
-        case ('rock'+'paper'): winner = 'player';break;  
-        case ('paper'+'scissor'): winner ='player';break;
-
-        case ('rock'+'rock'): winner = 'tie';break;
-        case ('paper'+'paper'): winner = 'tie';break;
-        case ('scissor'+'scissor'): winner = 'tie';break;
-        
-        default: console.log(`error:${computerSelection} ${playerSelection}`);break;
-    }
-
-    //congratulate the looser for winning
-    if (winner == 'player'){
-        console.log(`You win! ${playerSelection} beats ${computerSelection}.`);   
-    }else if (winner == 'computer'){
-        console.log(`You loose! ${computerSelection} beats ${playerSelection}.`);   
-    }else if(winner == 'tie'){
+    //determin winner
+    if (computerSelection == playerSelection){
         console.log(`Tie. No one wins. Idiot. 
-            ${computerSelection} beats ${playerSelection}.`);
+            computer:${computerSelection}  player:${playerSelection}.`);
+    }else if(beats(computerSelection) == playerSelection){
+        console.log(`You loose! ${computerSelection} beats ${playerSelection}.`);
+    }else if (beats(playerSelection == computerSelection)){
+        //what to do if player beats computer
+        console.log(`You win! ${playerSelection} beats ${computerSelection}.`);   
+    }else{        
+        console.log(`error determing winner
+            player:${playerSelection} 
+            computer:${computerSelection}`);
+    }
+    function beats(hand){
+        if(hand='scissor') return 'paper';
+        else if(hand = 'rock') return 'scissor';
+        else if(hand='paper') return 'rock';
     }
 }
 
